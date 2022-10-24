14097343fd04edbb3f197aa351de8bf5f9241b2f
ehdrua485 <ehdrua485@gmail.com>
2022-10-25 03:39:28 +0900

    comiit

--- Assets/Scripts/InGame/Inventory/PlayerStatUI.cs
+++ Assets/Scripts/InGame/Inventory/PlayerStatUI.cs
@@ -20,19 +20,19 @@ public class PlayerStatUI : MonoBehaviour
     {
         _str = "";
         _str += "--------���� ------\n";
-        _str += "���ݷ� : " + _stat.Attack + "\n";
-        _str += "���� : " + _stat.HitPercent + "\n";
-        _str += "��ų������ : " + _stat.SkillDamage + "%\n";
-        _str += "��絥���� : " + _stat.AllDamge + "%\n";
+        _str += "공격력 : " + _stat.Attack + "\n";
+        _str += "명중 : " + _stat.HitPercent + "\n";
+        _str += "스킬데미지 : " + _stat.SkillDamage + "%\n";
+        _str += "모든데미지 : " + _stat.AllDamge + "%\n";
         _str += "--------��� ------\n";
-        _str += "���� : " + _stat.Defense + "\n";
-        _str += "ȸ�� : " + _stat.Dodge + "\n";
-        _str += "�޴� ��� ������ ���� : " + _stat.ReduceDamage + "%\n";
+        _str += "방어력 :" + _stat.Defense + "\n";
+        _str += "회피 :  " + _stat.Dodge + "\n";
+        _str += "받는 모든 데미지 감소 : " + _stat.ReduceDamage + "%\n";
         _str += "--------��Ÿ ------\n";
-        _str += "�ִ� ä�� : " + _stat.MaxHp + "\n";
-        _str += "�ִ� ���� : " + _stat.MaxMp + "\n";
-        _str += "���� ���� ���� : " + _stat.MaxPostion + "\n";
-        _str += "HPȸ���� : " + _stat.RecoveryHp + "\n";
-        _str += "MPȸ���� : " + _stat.RecoveryMp + "\n";
+        _str += "최대 채력 : " + _stat.MaxHp + "\n";
+        _str += "최대 엠피 : " + _stat.MaxMp + "\n";
+        _str += "물약 소지 개수 : " + _stat.MaxPostion + "\n";
+        _str += "HP회복량 : " + _stat.RecoveryHp + "\n";
+        _str += "MP회복량 : " + _stat.RecoveryMp + "\n";
     }
 }
