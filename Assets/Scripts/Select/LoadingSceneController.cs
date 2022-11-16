7ce3305551612fb5087d275eb5c516a765d00df2
hyeon0316 <92031289+hyeon0316@users.noreply.github.com>
2022-11-17 03:09:35 +0900

    Update

--- Assets/Scripts/Select/LoadingSceneController.cs
+++ Assets/Scripts/Select/LoadingSceneController.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LoadingSceneController : MonoBehaviour
 {
@@ -10,6 +11,7 @@ public class LoadingSceneController : MonoBehaviour
     [SerializeField] private string _sceneName;
     private float _time;
 
+    [SerializeField] private Image _loadingBar;
 
     public void LoadAsyncScene()
     {
@@ -25,6 +27,8 @@ public class LoadingSceneController : MonoBehaviour
         while (!operation.isDone)//씬 호출되기 전 까지 반복
         {
             _time += Time.deltaTime;
+
+            _loadingBar.fillAmount = Mathf.Lerp(_loadingBar.fillAmount, operation.progress, _time/ Global.LoadingTime);
 
             if (_time >= Global.LoadingTime)
                 operation.allowSceneActivation = true;
